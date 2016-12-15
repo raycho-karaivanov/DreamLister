6aba0a37792ecdfa9c019f04cceb4937ecbb8dba
Raycho Karaivanov <raycho.karaivanov@gmail.com>
2016-12-15 11:14:39 +0200

    going forward
    going forward

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Raycho Karaivanov on 15/12/2016.
+//  Copyright © 2016 Raycho Karaivanov. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var priceLbl: UILabel!
+    @IBOutlet weak var detailsLbl: UILabel!
+}
---
+++ DreamLister/MainVC.swift
@@ -0,0 +1,41 @@
+//
+//  MainVC.swift
+//  DreamLister
+//
+//  Created by Raycho Karaivanov on 14/12/2016.
+//  Copyright © 2016 Raycho Karaivanov. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var segment: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+}
+
